ca35b6c51c9dc3a31e07ac9e446dddb1184a003b
Reinier de la Parra <reinierdlp@gmail.com>
2019-01-30 15:53:29 +0100

    information panel

---
+++ src/nl/hanze/Windows/InformationPanel.java
@@ -0,0 +1,47 @@
+package nl.hanze.Windows;
+
+import nl.hanze.Main;
+import nl.hanze.controllers.FloorController;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class InformationPanel extends JPanel {
+
+    public static JLabel quene1, quene2, quene3, quene4, quene5, quene6, quene7, quene8;
+
+    public InformationPanel(FloorController controller) {
+        super();
+
+        JPanel informationPanel = new JPanel();
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+
+
+
+        InformationPanel.quene1 = new JLabel("Totaal spots: " + controller.getNumberOfOpenSpots());
+        this.add(InformationPanel.quene1);
+
+        InformationPanel.quene2 = new JLabel("Member spots: " + controller.getNumberOfOpenSpots());
+        this.add(InformationPanel.quene2);
+
+        InformationPanel.quene3 = new JLabel("Totaal spots none: " + controller.getNumberOfOpenSpots());
+        this.add(InformationPanel.quene3);
+
+        InformationPanel.quene4 = new JLabel("Totaal spots reservered: " + controller.getNumberOfOpenSpots());
+        this.add(InformationPanel.quene4);
+
+        InformationPanel.quene5 = new JLabel("Mem lived: " + controller.getNumberOfOpenSpots());
+        this.add(InformationPanel.quene5);
+
+        InformationPanel.quene6 = new JLabel("None lived: " + controller.getNumberOfOpenSpots());
+        this.add(InformationPanel.quene6);
+
+        InformationPanel.quene7 = new JLabel("Reservations none: " + controller.getNumberOfOpenSpots());
+        this.add(InformationPanel.quene7);
+
+        setVisible(true);
+
+        informationPanel.setMaximumSize(new Dimension(100, 100));
+    }
+}
--- src/nl/hanze/Windows/MainWindow.java
+++ src/nl/hanze/Windows/MainWindow.java
@@ -43,45 +43,10 @@ public class MainWindow extends JFrame {
         panel.add(floorController.getView(FloorType.FLOOR_TYPE_NONE),BorderLayout.CENTER);
         panel.add(floorController.getView(FloorType.FLOOR_TYPE_RESAVERED),BorderLayout.EAST);
 
-
-        JPanel informationPanel = new JPanel();
-        informationPanel.setLayout(new BoxLayout(informationPanel, BoxLayout.Y_AXIS));
-        Main.label1 = new JLabel("Totaal spots: " + floorController.getNumberOfOpenSpots());
-        informationPanel.add(Main.label1);
-
-        Main.label2 = new JLabel("Member spots: " + floorController.getNumberOfOpenSpots());
-        informationPanel.add(Main.label2);
-
-        Main.label3 = new JLabel("Totaal spots none: " + floorController.getNumberOfOpenSpots());
-        informationPanel.add(Main.label3);
-
-        Main.label4 = new JLabel("Totaal spots reservered: " + floorController.getNumberOfOpenSpots());
-        informationPanel.add(Main.label4);
-
-        Main.label5 = new JLabel("Mem lived: " + floorController.getNumberOfOpenSpots());
-        informationPanel.add(Main.label1);
-
-        Main.label6 = new JLabel("None lived: " + floorController.getNumberOfOpenSpots());
-        informationPanel.add(Main.label2);
-
-        Main.label7 = new JLabel("Reservations none: " + floorController.getNumberOfOpenSpots());
-        informationPanel.add(Main.label3);
-
-        Main.label8 = new JLabel();
-
-        informationPanel.add(this.quene1);
-        informationPanel.add(this.quene2);
-        informationPanel.add(this.quene3);
-        informationPanel.add(this.quene4);
-        informationPanel.add(Main.label5);
-        informationPanel.add(Main.label6);
-        informationPanel.add(Main.label7);
-
-        informationPanel.setMaximumSize(new Dimension(100,100));
-
         scrollPane.getViewport().addChangeListener((e) -> { this.repaint(); } );
 
         container.add(scrollPane,BorderLayout.EAST);
+        InformationPanel informationPanel = new InformationPanel(floorController);
         container.add(informationPanel,BorderLayout.WEST);
 
         JPanel p = new JPanel();
@@ -91,17 +56,10 @@ public class MainWindow extends JFrame {
         p.add(informationPanel);
         this.add(p);
 
-
         setSize(800, 600);
-
-
 
         this.statusBar = new StatusBar();
         this.getContentPane().add(this.statusBar, java.awt.BorderLayout.SOUTH);
-//
-//        this.setSize(800,600);
-//        scrollPane.setSize(400,300);
-
         this.setVisible(true);
 
         Simulator simulator = new Simulator(floorController, this);
